72f4aacc79429acbfb205ecee53bcc170ab051f5
Adi Chirilov <developer@rosoftware.ro>
2019-06-14 17:20:30 +0300

    App CleanUp

---
+++ run.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""
+Author: AppSeed App Generator
+License: MIT
+"""
+
+# used by the static export
+import click
+from   flask_frozen import Freezer
+
+from app import app
+from app import db
+
+# define custom command 
+@app.cli.command()
+def build():
+    freezer = Freezer(app)
+    freezer.freeze()
+
+if __name__ == "__main__":
+
+    db.create_all()
+    app.run() 
